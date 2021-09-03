2767b51774e94bcdfa9954490858de3a2886b6eb
Matthew Dailey <dailey.matthew@gmail.com>
2021-09-03 11:57:17 +0700

    Add Devise for authentication

---
+++ studentdb/config/deploy.rb
@@ -0,0 +1,19 @@
+# config valid for current version and patch releases of Capistrano
+lock "~> 3.16.0"
+
+set :application, "studentdb"
+set :repo_url, "git@gitlab.com:ait-wae-2021/web16/web16-app"
+set :rbenv_type, :user
+set :rbenv_ruby, '3.0.2'
+set :repo_tree, 'studentdb'
+
+
+
+append :linked_files, "config/database.yml", "config/master.key"
+
+append :linked_dirs, "log", "tmp", "public/system", "public/assets", "public/packs", ".bundle"
+
+set :keep_releases, 5
+
+after 'deploy:publishing', 'puma:restart'
+
---
+++ studentdb/config/deploy/production.rb
@@ -0,0 +1,11 @@
+
+set :default_env, {'http_proxy'=>'192.41.170.23:3128',
+                   'https_proxy'=>'192.41.170.23:3128',
+                   'BAZOOKA_USER' => ENV['USER'] }
+
+server "web16", user: "deploy", roles: %w{app db web}
+
+set :deploy_to, "/home/deploy/studentdb"
+
+set :yarn_flags, '--production --silent --no-progress --network-timeout 1000000'
+
