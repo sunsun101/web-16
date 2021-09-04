efbe533920f95d63ac8f0821dfc5d0d430841ce5
Matthew Dailey <dailey.matthew@gmail.com>
2021-09-04 11:50:51 +0700

    Specify URL for mailer in production

--- studentdb/config/environments/production.rb
+++ studentdb/config/environments/production.rb
@@ -64,6 +64,9 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  # Default mailer url for Devise
+  config.action_mailer.default_url_options = { host: 'http://web16.cs.ait.ac.th' }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
