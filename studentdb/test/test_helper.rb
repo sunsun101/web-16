e6967ce66f248fd709e9c890ad58fbf2bd068521
Matthew Dailey <dailey.matthew@gmail.com>
2021-09-08 13:08:09 +0700

    Fix position of simplecov gem load in test

--- studentdb/test/test_helper.rb
+++ studentdb/test/test_helper.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
+require 'simplecov'
+SimpleCov.start 'rails'
+
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
-require 'simplecov'
-SimpleCov.start 'rails'
 
 module ActiveSupport
   class TestCase
